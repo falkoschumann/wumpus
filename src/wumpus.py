e8d01eed48c53edb16ff715b3b37f5cdc657562f
Falko Schumann <falko.schumann@muspellheim.de>
2024-05-04 09:26:35 +0200

    feat: print location and hazard warnings

--- src/wumpus.py
+++ src/wumpus.py
@@ -3,12 +3,12 @@ import random
 
 class Game:
     def __init__(self):
-        self.s = []
-        self.l = []
-        self.m = []
-        self.a = 0
+        self.s = []  # cave: 20 rooms, 3 tunnels per room
+        self.l = []  # rooms: 1 you, 2 wumpus, 3 & 4 pits, 5 & 6 bats
+        self.m = []  # memory of l
+        self.a = 0  # number of arrows
 
-    def display_instructions(self):
+    def print_instructions(self):
         print("Welcome to 'Hunt the Wumpus'")
         print("  The wumpus lives in a cave of 20 rooms. each room")
         print("has 3 tunnels leading to other rooms. (Look at a")
@@ -52,26 +52,26 @@ class Game:
 
     def setup_cave(self):
         self.s = [
-            [2, 5, 8],
-            [1, 3, 10],
-            [2, 4, 12],
-            [3, 5, 14],
-            [1, 4, 6],
-            [5, 7, 15],
-            [6, 8, 17],
-            [1, 7, 9],
-            [8, 10, 18],
-            [2, 9, 11],
-            [10, 12, 19],
-            [3, 11, 13],
-            [12, 14, 20],
-            [4, 13, 15],
-            [6, 14, 16],
-            [15, 17, 20],
-            [7, 16, 18],
-            [9, 17, 19],
-            [11, 18, 20],
-            [13, 16, 19],
+            [1, 4, 7],  # 0
+            [0, 2, 9],  # 1
+            [1, 3, 11],  # 2
+            [2, 4, 13],  # 3
+            [0, 3, 5],  # 4
+            [4, 6, 14],  # 5
+            [5, 7, 16],  # 6
+            [0, 6, 8],  # 7
+            [7, 9, 17],  # 8
+            [1, 8, 10],  # 9
+            [9, 11, 18],  # 10
+            [2, 10, 12],  # 11
+            [11, 13, 19],  # 12
+            [3, 12, 14],  # 13
+            [5, 13, 15],  # 14
+            [14, 16, 19],  # 15
+            [6, 15, 17],  # 16
+            [8, 16, 18],  # 17
+            [10, 17, 19],  # 18
+            [12, 15, 18],  # 19
         ]
 
     def fna(self):
@@ -103,19 +103,39 @@ class Game:
 
     def print_location_and_hazard_warnings(self):
         print()
+        for j in range(1, 6):
+            for k in range(0, 3):
+                if self.s[self.l[0]][k] != self.l[j]:
+                    continue
+                if j == 1:
+                    print("I smell a wumpus")
+                elif j == 2 or j == 3:
+                    print("I feel a draft")
+                else:
+                    print("Bats nearby")
+        print("You are in room", self.l[0])
+        print(
+            "Tunnels lead to rooms",
+            self.s[self.l[0]][0],
+            self.s[self.l[0]][1],
+            self.s[self.l[0]][2],
+        )
+        print()
 
 
 def main():
     game = Game()
     i = input("Instructions (y/n)? ")
-    if i == "y":
-        game.display_instructions()
+    if i != "n":
+        game.print_instructions()
     game.setup_cave()
     game.locate_items()
     game.set_number_of_arrows()
     l = game.l[0]
+
+    # Start the game
     print("Hunt the Wumpus")
-    game.print_location_and_hazard_warnings()  # 380
+    game.print_location_and_hazard_warnings()
 
 
 if __name__ == "__main__":
